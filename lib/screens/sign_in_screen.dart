324dd9567b7ab0cf6783a4f789f83cf105ecaba3
iambrianwalker <bwalker37837@gmail.com>
2026-04-22 20:28:38 -0400

    feat: added widget cleaning logic and testing for username and password reception

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -11,4 +11,21 @@ class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void loginUser() {
+    String username = usernameController.text.trim();
+    String password = passwordController.text.trim();
+
+    debugPrint("Username: $username");
+    debugPrint("Password: $password");
+
+    // Add Firebase login here later
+  }
+
 }

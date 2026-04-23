7686324fb54a58e94215cebd4d15f88a3c5ce5da
iambrianwalker <bwalker37837@gmail.com>
2026-04-22 20:26:52 -0400

    feat: added text controllers and created stateful state for screen

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+}

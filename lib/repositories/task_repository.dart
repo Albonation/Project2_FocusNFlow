4441af7294bd817cbb18c13565ec4ea9e9cb2d3b
Alan Pruett <cpruett3@student.gsu.edu>
2026-04-23 16:15:50 -0400

    chore: create files for task service and task repository

---
+++ lib/repositories/task_repository.dart
@@ -0,0 +1,2 @@
+//connect directly with firestore and get data from there
+//all the CRUD operations for tasks will be here
---
+++ lib/services/task_service.dart
@@ -0,0 +1,2 @@
+//whether a task can be completed, completing a task, validating task data
+//essentially the rules and logic for the task system

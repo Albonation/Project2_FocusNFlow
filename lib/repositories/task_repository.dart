e901b3d9baab047698f5a10146285a66ef3e4a02
Alan Pruett <cpruett3@student.gsu.edu>
2026-04-23 17:11:30 -0400

    feat: implement addTask in task repository and add method headers for planned methods

--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -2,28 +2,62 @@
 //all the CRUD operations for tasks will be here
 
 //importing necessary packages
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import '../models/task_model.dart';
 
-//add task
+class TaskRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-//update task
+  CollectionReference<Map<String, dynamic>> get _tasksCollection =>
+      _firestore.collection('tasks');
 
-//delete task
+  //add task and return the generated document ID
+  Future<String> addTask(Task task) async {
+    try {
+      DocumentReference<Map<String, dynamic>> docRef = await _tasksCollection
+          .add(task.toMap());
+      debugPrint('[TASK_REPO] Added task: ${docRef.id}');
+      return docRef.id; //
+    } catch (e) {
+      debugPrint('[TASK_REPO] Failed to add task: $e');
+      rethrow;
+    }
+  }
 
-//read tasks, or stream tasks since using firestore
+  //read tasks, or stream tasks since using firestore
+  //get all tasks for a specific user
+  Stream<List<Task>> getTasksForUser(String userId) {
+    throw UnimplementedError();
+  }
 
-//get all tasks for a specific user
+  //get a specific task by id
+  Future<Task?> getTaskById(String taskId) async {
+    throw UnimplementedError();
+  }
 
-//get a specific task by id
+  //update task
+  Future<void> updateTask(Task task) async {
+    throw UnimplementedError();
+  }
 
-//some maybe methods
-/*
+  //delete task
+  Future<void> deleteTask(String taskId) async {
+    throw UnimplementedError();
+  }
 
-//get tasks by status
+  //some maybe methods
+  /*
 
-//get tasks by course id
+  //get tasks by status
 
-//get tasks by deadline
+  //get tasks by course id
 
-//get tasks by estimated hours
+  //get tasks by deadline
 
-*/
+  //get tasks by estimated hours
+
+  //maybe subtask stuffs
+
+  */
+} //end of class

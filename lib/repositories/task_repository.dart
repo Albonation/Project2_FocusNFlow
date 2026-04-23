b28706a16f0b30828847ada328a55e9944f3585f
Alan Pruett <cpruett3@student.gsu.edu>
2026-04-23 16:27:30 -0400

    feat: brainstorm task repository and task service using comments for planned methods

--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -1,2 +1,29 @@
 //connect directly with firestore and get data from there
 //all the CRUD operations for tasks will be here
+
+//importing necessary packages
+
+//add task
+
+//update task
+
+//delete task
+
+//read tasks, or stream tasks since using firestore
+
+//get all tasks for a specific user
+
+//get a specific task by id
+
+//some maybe methods
+/*
+
+//get tasks by status
+
+//get tasks by course id
+
+//get tasks by deadline
+
+//get tasks by estimated hours
+
+*/
--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,2 +1,10 @@
 //whether a task can be completed, completing a task, validating task data
 //essentially the rules and logic for the task system
+
+//importing necessarry packages
+
+//complete a task
+
+//validate a task
+
+//check if a task can be completed

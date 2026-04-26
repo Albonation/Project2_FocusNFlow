a671758c81273a014c292002bb3054e31159e544
iambrianwalker <bwalker37837@gmail.com>
2026-04-26 15:36:49 -0400

    feat: moved weekly planner service plan to service file

--- lib/services/weekly_planner_service.dart
+++ lib/services/weekly_planner_service.dart
@@ -1,3 +1,23 @@
+import 'package:focus_n_flow/models/task_model.dart';
 
+class WeeklyPlannerService {
+
+  Map<String, List<Task>> generateWeeklyPlan(List<Task> tasks) {
+    final days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    final sorted = [...tasks];
+
+    sorted.sort((a, b) =>
+        b.priorityScore.compareTo(a.priorityScore));
+
+    final plan = <String, List<Task>>{};
+
+    for (int i = 0; i < sorted.length; i++) {
+      final day = days[i % 7];
+      plan.putIfAbsent(day, () => []);
+      plan[day]!.add(sorted[i]);
+    }
+
+    return plan;
   }
 }

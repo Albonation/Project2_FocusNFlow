c5c6c42eb5b84b71038f3fd3bfad3476b6978f51
iambrianwalker <bwalker37837@gmail.com>
2026-04-26 15:39:01 -0400

    fix: added planner widget to weekly planner screen. configuring for testing

--- lib/screens/weekly_planner_screen.dart
+++ lib/screens/weekly_planner_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:focus_n_flow/repositories/task_repository.dart';
 import 'package:focus_n_flow/services/weekly_planner_service.dart';
-import 'package:focus_n_flow/widgets/weekly_planner_widgets/weekly_plan_section.dart';
+import 'package:focus_n_flow/widgets/weekly_planner_widgets/weekly_planner_widget.dart';
 
 class WeeklyPlannerScreen extends StatefulWidget {
   const WeeklyPlannerScreen({super.key});
@@ -29,45 +29,7 @@ class _WeeklyPlannerScreenState extends State<WeeklyPlannerScreen> {
       appBar: AppBar(
         title: const Text("Weekly Study Plan"),
       ),
-      body: WeeklyPlanSection(
-        userId: user.uid,
-        repository: _repository,
-        service: _service,
-      ),
-    );
-  }
-}import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:focus_n_flow/repositories/task_repository.dart';
-import 'package:focus_n_flow/services/weekly_planner_service.dart';
-import 'package:focus_n_flow/widgets/weekly_planner_widgets/weekly_plan_section.dart';
-
-class WeeklyPlannerScreen extends StatefulWidget {
-  const WeeklyPlannerScreen({super.key});
-
-  @override
-  State<WeeklyPlannerScreen> createState() => _WeeklyPlannerScreenState();
-}
-
-class _WeeklyPlannerScreenState extends State<WeeklyPlannerScreen> {
-  final TaskRepository _repository = TaskRepository();
-  final WeeklyPlannerService _service = WeeklyPlannerService();
-
-  @override
-  Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-
-    if (user == null) {
-      return const Scaffold(
-        body: Center(child: Text("No user logged in")),
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Weekly Study Plan"),
-      ),
-      body: WeeklyPlanSection(
+      body: WeeklyPlannerWidget(
         userId: user.uid,
         repository: _repository,
         service: _service,

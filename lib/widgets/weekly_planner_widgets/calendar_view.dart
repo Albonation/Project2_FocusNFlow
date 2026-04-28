6cd4bd713cb9b83c9e423d4a21d30dde55fbbe90
iambrianwalker <bwalker37837@gmail.com>
2026-04-28 12:43:55 -0400

    feat: added expanded list view for tasks when day is selected

--- lib/widgets/weekly_planner_widgets/calendar_view.dart
+++ lib/widgets/weekly_planner_widgets/calendar_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:focus_n_flow/models/task_model.dart';
 import 'package:focus_n_flow/services/weekly_planner_service.dart';
-import 'package:focus_n_flow/theme/app_spacing.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPlannerView extends StatefulWidget {
@@ -23,19 +22,19 @@ class _CalendarPlannerViewState extends State<CalendarPlannerView> {
   DateTime? selectedDay;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final dayTasks = selectedDay == null
-      ? widget.tasks
-      : widget.service.filterTaskByDate(widget.tasks, selectedDay!);
+        ? widget.tasks
+        : widget.service.filterTasksByDate(widget.tasks, selectedDay!);
 
-    return Columm(
+    return Column(
       children: [
         TableCalendar(
           firstDay: DateTime.utc(2020),
           lastDay: DateTime.utc(2030),
           focusedDay: focusedDay,
           selectedDayPredicate: (day) => isSameDay(selectedDay, day),
-          onDaySelected: (selected, focused){
+          onDaySelected: (selected, focused) {
             setState(() {
               selectedDay = selected;
               focusedDay = focused;
@@ -47,10 +46,22 @@ class _CalendarPlannerViewState extends State<CalendarPlannerView> {
 
         Expanded(
           child: ListView(
-            
+            children: dayTasks.map((task) {
+              return ListTile(
+                title: Text(task.title),
+                subtitle: Text("Due: ${task.deadline}"),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(task.priorityScore.toStringAsFixed(1)),
+                    const Text("Priority", style: TextStyle(fontSize: 10)),
+                  ],
+                ),
+              );
+            }).toList(),
           ),
-        )
-      ]
-    )
+        ),
+      ],
+    );
   }
 }

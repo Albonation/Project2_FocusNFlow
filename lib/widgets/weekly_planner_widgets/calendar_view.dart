71c04f0ec2ab1b04f7fbc2611820abd097f70aae
iambrianwalker <bwalker37837@gmail.com>
2026-04-28 12:38:03 -0400

    feat: added calendar day selection for tasks on specific days

--- lib/widgets/weekly_planner_widgets/calendar_view.dart
+++ lib/widgets/weekly_planner_widgets/calendar_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:focus_n_flow/models/task_model.dart';
 import 'package:focus_n_flow/services/weekly_planner_service.dart';
+import 'package:focus_n_flow/theme/app_spacing.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPlannerView extends StatefulWidget {
   final List<Task> tasks;
@@ -21,5 +23,34 @@ class _CalendarPlannerViewState extends State<CalendarPlannerView> {
   DateTime? selectedDay;
 
   @override
-  
+  Widget build(BuildContext context){
+    final dayTasks = selectedDay == null
+      ? widget.tasks
+      : widget.service.filterTaskByDate(widget.tasks, selectedDay!);
+
+    return Columm(
+      children: [
+        TableCalendar(
+          firstDay: DateTime.utc(2020),
+          lastDay: DateTime.utc(2030),
+          focusedDay: focusedDay,
+          selectedDayPredicate: (day) => isSameDay(selectedDay, day),
+          onDaySelected: (selected, focused){
+            setState(() {
+              selectedDay = selected;
+              focusedDay = focused;
+            });
+          },
+        ),
+
+        const SizedBox(height: 10),
+
+        Expanded(
+          child: ListView(
+            
+          ),
+        )
+      ]
+    )
+  }
 }

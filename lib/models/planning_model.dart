20d32d24f0370e6942d8c275c18838858df75bc2
iambrianwalker <bwalker37837@gmail.com>
2026-04-30 12:12:53 -0400

    feat: added new required variable for planned task model and added to and from map functions

--- lib/models/planning_model.dart
+++ lib/models/planning_model.dart
@@ -1,11 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:focus_n_flow/models/task_model.dart';
 
 class PlannedTask{
   final Task task;
   final double hoursForDay;
+  final DateTime plannedDate;
 
   PlannedTask({
     required this.task,
     required this.hoursForDay,
+    required this.plannedDate,
   });
+
+  Map<String, dynamic> toMap(){
+    return {
+      'task_id': task.id,
+      'hours_for_day': hoursForDay,
+      'date': Timestamp.fromDate(plannedDate),
+    };
+  }
+
+  factory PlannedTask.fromMap(
+    Map<String, dynamic> map,
+    Task task,
+  ) {
+    return PlannedTask(
+      task: task,
+      hoursForDay: (map['hours_for_day'] as num).toDouble(),
+      plannedDate: (map['planned_date'] as Timestamp).toDate(),
+    );
+  }
 }

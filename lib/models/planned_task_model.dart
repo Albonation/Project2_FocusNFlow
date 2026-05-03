adf8346818b64f27febf614bfe6a71ce5ceba54a
iambrianwalker <bwalker37837@gmail.com>
2026-05-03 11:42:13 -0400

    feat: added functionality to model to select plans as complete

--- lib/models/planned_task_model.dart
+++ lib/models/planned_task_model.dart
@@ -6,6 +6,7 @@ class PlannedTask {
   final String courseId;
   final DateTime date;
   final int unitIndex;
+  final bool isCompleted;
 
   PlannedTask({
     required this.taskId,
@@ -13,8 +14,26 @@ class PlannedTask {
     required this.courseId,
     required this.date,
     required this.unitIndex,
+    this.isCompleted = false
   });
 
+  PlannedTask copyWith({
+    String? taskId,
+    String? title,
+    String? courseId,
+    DateTime? date,
+    int? unitIndex,
+    bool? isCompleted,
+  }) {
+    return PlannedTask(
+      taskId: taskId ?? this.taskId,
+      title: title ?? this.title,
+      courseId: courseId ?? this.courseId,
+      date: date ?? this.date,
+      unitIndex: unitIndex ?? this.unitIndex,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
   
   Map<String, dynamic> toMap() {
     return {
@@ -23,6 +42,7 @@ class PlannedTask {
       'course_id': courseId,
       'date': Timestamp.fromDate(date),
       'unit_index': unitIndex,
+      'is_completed': isCompleted,
     };
   }
 
@@ -34,6 +54,7 @@ class PlannedTask {
       courseId: map['course_id'] ?? '',
       date: (map['date'] as Timestamp).toDate(),
       unitIndex: map['unit_index'] ?? 0,
+      isCompleted: map['is_completed'] ?? false,
     );
   }
 }
